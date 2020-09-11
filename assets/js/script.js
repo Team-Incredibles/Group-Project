a7eda795289cabd6b54119e217e15bea4ee2d169
Ender <woodwind.turbeville@gmail.com>
2020-09-11 02:39:36 -0500

    Added new popover from css framework to inform Users of what the card does, only for larger displays, adjusted website description, and detailed code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var nasaKey = 'QFBaySAYAbefXA8kgBoxfYWOYqWKcnmCMXq58czU'
 
-//Weather section
+// Weather section
 $('#add-city').on('click', function() { 
     event.preventDefault();
     console.log('Weather button clicked');
@@ -9,27 +9,27 @@ $('#add-city').on('click', function() {
 
 
 
-//IOTD Section
+// IOTD Section
 var getIotd = function() {
-    //gets the image of the day and its corresponding information
+    // Gets the image of the day and its corresponding information
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`)
     .then(function(response) {
         
         if (response.ok) {
             response.json().then(function (data) {
                 
-                //gets the data we need and stores them in variables
+                // Gets the data we need and stores them in variables
                 var description = data.explanation
                 var title = data.title
                 var picOtd = data.hdurl
                 
-                //passes that data to the iotd() function
+                // Passes that data to the iotd() function
                 iotd(description, title, picOtd)
             
             });
 
         } else {
-            //if for some reason is not valid an error message is displayed
+            // If for some reason is not valid an error message is displayed
             $('#imgOtd').attr('src', './assets/images/alien.png')
             $('#image-description').text("Sorry! looks like NASA doesn't have an image for today.")
             $('#iotdTitle').text('')
@@ -41,24 +41,23 @@ var getIotd = function() {
 };
 
 var iotd = function(description, title, picOtd) {
-    //puts data from the getIotd() function in its correct places on the page
+    // Puts data from the getIotd() function in its correct places on the page
     $('#imgOtd').attr('src', picOtd)
     $('#image-description').text(description)
     $('#iotdTitle').text(title)
     $('#toIotd').attr('href', picOtd)
 
 }
-//end IOTD section
+// End IOTD section
 
-//mars weather section
+// Mars weather section
 var getMars = function() {
     
-    //get horizontal wind speed
-    //get high, low, and avg temp
-    //get season
-    //get sol and earth date
+    // Get horizontal wind speed
+    // Get high, low, and avg temp
+    // Get season
+    // Get sol and earth date
 
 }
-
 
 getIotd();
