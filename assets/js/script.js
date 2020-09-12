aad09c3bff05cc892a1195a13b79b6fbad42f134
greyw <greywhitt@gmail.com>
2020-09-12 15:57:45 -0500

    resolving merge conflicts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var today = moment().format('YYYY-MM-DD')
 var imgProgress = document.querySelector('#loadingCircle');
 
 // Weather section
-$('#add-city').on('click', function() { 
+$('#add-city').on('click', function() {
     event.preventDefault();
     console.log('Weather button clicked');
 });
@@ -12,23 +12,23 @@ $('#add-city').on('click', function() {
 
 
 // IOTD Section
-var getIotd = function() {
+$(document).ready(function() {
     // Gets the image of the day and its corresponding information
     fetch(`https://api.nasa.gov/planetary/apod?date=${today}&api_key=${nasaKey}`)
     .then(function(response) {
-        
+    
         if (response.ok) {
             response.json().then(function (data) {
                 
-                // Gets the data we need and stores them in variables
-                var description = data.explanation
-                var title = data.title
-                var picOtd = data.hdurl
+            // Gets the data we need and stores them in variables
+            var description = data.explanation
+            var title = data.title
+            var picOtd = data.hdurl
                 
-                // Passes that data to the iotd() function
-                iotd(description, title, picOtd)
+            // Passes that data to the iotd() function
+            iotd(description, title, picOtd)
             
-            });
+        });
 
         } else {
             // If for any reason the request is not valid an error message is displayed
@@ -41,17 +41,17 @@ var getIotd = function() {
         // To notify user of connection issue to Nasa API
         $('#image-description').text("Sorry! It looks like your device is having issues connecting to Nasa's API. Retry your connection or come back later! Thank you.")
         $('#iotdTitle').text('Error: ' + status.statusText);
-    })
-};
 
-var iotd = function(description, title, picOtd) {
-    // Puts data from the getIotd() function in its correct places on the page
-    $('#imgOtd').attr('src', picOtd)
-    $('#image-description').text(description)
-    $('#iotdTitle').text(title)
-    $('#toIotd').attr('href', picOtd)
+    });
 
-}
+    var iotd = function(description, title, picOtd) {
+        // Puts data from the getIotd() function in its correct places on the page
+        $('#imgOtd').attr('src', picOtd)
+        $('#image-description').text(description)
+        $('#iotdTitle').text(title)
+        $('#toIotd').attr('href', picOtd)
+    }
+});
 // End IOTD section
 
 // Mars weather section
