3a49a01d2947ca8bcb989970a7d6d80251babf15
Ender <woodwind.turbeville@gmail.com>
2020-09-11 20:01:08 -0500

    Added adjustments to footer in css & html, Added Error Handlingin JS and tested all functionality, added Asteroid Section, and detailed JS to prepare for further improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,72 @@
+// Global Variables
 var nasaKey = 'QFBaySAYAbefXA8kgBoxfYWOYqWKcnmCMXq58czU'
+var imgProgress = document.querySelector('#loadingCircle');
 
-// Weather section
-$('#add-city').on('click', function() { 
-    event.preventDefault();
-    console.log('Weather button clicked');
-});
+// Date format for API
+// Current Date from Moment
+// Temporary > Can be deleted if never used in project
+const now = moment().format('YYYY-MM-DD');
 
-
-
+// To show a Loading Bar while POTD is being fetched and returned
+document.onreadystatechange = function() {
+    if (document.readyState === 'complete') {
+        imgProgress.style.display = 'none';
+    }
+}
 
 // IOTD Section
-var getIotd = function() {
+$(document).ready(function() {
     // Gets the image of the day and its corresponding information
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`)
     .then(function(response) {
-        
+    
         if (response.ok) {
             response.json().then(function (data) {
                 
-                // Gets the data we need and stores them in variables
-                var description = data.explanation
-                var title = data.title
-                var picOtd = data.hdurl
+            // Gets the data we need and stores them in variables
+            var description = data.explanation
+            var title = data.title
+            var picOtd = data.hdurl
                 
-                // Passes that data to the iotd() function
-                iotd(description, title, picOtd)
+            // Passes that data to the iotd() function
+            iotd(description, title, picOtd)
             
-            });
+        });
 
         } else {
             // If for some reason is not valid an error message is displayed
             $('#imgOtd').attr('src', './assets/images/alien.png')
-            $('#image-description').text("Sorry! looks like NASA doesn't have an image for today.")
-            $('#iotdTitle').text('')
-        };
-
-
+            $('#image-description').text("Sorry! It looks like NASA doesn't have an image for today.")
+            $('#iotdTitle').text(status.statusText)
+        }
+    })
+    .catch(function(error) {
+        // To notify user of connection issue to Nasa API
+        $('#image-description').text("Sorry! It looks like your device is having issues connecting to Nasa's API. Retry your connection or come back later! Thank you.")
+        $('#iotdTitle').text('Error: ' + status.statusText);
     });
 
-};
-
-var iotd = function(description, title, picOtd) {
-    // Puts data from the getIotd() function in its correct places on the page
-    $('#imgOtd').attr('src', picOtd)
-    $('#image-description').text(description)
-    $('#iotdTitle').text(title)
-    $('#toIotd').attr('href', picOtd)
-
-}
+    var iotd = function(description, title, picOtd) {
+        // Puts data from the getIotd() function in its correct places on the page
+        $('#imgOtd').attr('src', picOtd)
+        $('#image-description').text(description)
+        $('#iotdTitle').text(title)
+        $('#toIotd').attr('href', picOtd)
+    }
+});
 // End IOTD section
+
+// Asteroid Section
+$('#add-asteroid').on('click', function () {
+    event.preventDefault();
+    console.log('Asteroid button clicked');
+});
+
+// Mars Weather section
+$('#add-weather').on('click', function() { 
+    event.preventDefault();
+    console.log('Weather button clicked');
+});
 
 // Mars weather section
 var getMars = function() {
@@ -57,7 +75,4 @@ var getMars = function() {
     // Get high, low, and avg temp
     // Get season
     // Get sol and earth date
-
 }
-
-getIotd();
