44021f782e7d81c501fd0a55bbc841fb76fb3efb
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-16 23:18:53 -0500

    created variables in js

---
+++ assets/js/resources.js
@@ -0,0 +1,6 @@
+var remember = document.querySelector(".remember");
+var forget = document.querySelector(".forget");
+var form = document.querySelector('form');
+var message = document.querySelector('#message');
+var submit = document.querySelector('#submit');
+var erase = document.querySelector('#delete');
